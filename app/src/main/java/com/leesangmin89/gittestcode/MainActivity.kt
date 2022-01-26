bd67f1b2aa07a76344732d31287e3d60adeff61f
Jake Lee <huny357@naver.com>
2022-01-26 14:22:05 +0900

    Initial commit

---
+++ app/src/main/java/com/leesangmin89/gittestcode/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.leesangmin89.gittestcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "연습1", Toast.LENGTH_SHORT).show()
+    }
+}
